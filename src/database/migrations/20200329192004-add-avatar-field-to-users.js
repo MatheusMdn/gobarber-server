7960220a87bb4ac9a5d81f57927353d9b96c0daf
Matheusdn <matheusmartinsdn@gmail.com>
2020-04-19 10:47:54 -0300

    Backend

---
+++ src/database/migrations/20200329192004-add-avatar-field-to-users.js
@@ -0,0 +1,16 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'files',
+        key: 'id',
+      },
+      allowNull: true,
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
